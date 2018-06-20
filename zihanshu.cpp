b0a77a16d689316289b4d95ebc0a6ce748aa6262
zf614032464 <40415550+zf614032464@users.noreply.github.com>
2018-06-20 23:22:50 +0800

    Add files via upload

--- zihanshu.cpp
+++ zihanshu.cpp
@@ -9,7 +9,7 @@ void read(zhigong zggz[],int a)//文件读取
 	}
 	for(int i=0;i<a;i++)
 	{
-		n=n+1;//职工人数加1
+		n++;//职工人数加1
 		f>>zggz[i].id>>zggz[i].name>>zggz[i].gangwei>>zggz[i].xinji>>zggz[i].zhiwu>>zggz[i].jixiao>>zggz[i].yingfa>>zggz[i].geren>>zggz[i].shifa;
 	}
 	f.close();
@@ -155,43 +155,87 @@ void del(zhigong zggz[])//删除函数
 
 void add(zhigong *p)//增加函数
 {
+	if(n==100)
+	{
+		cout<<"职工人数已满，不能添加职工了"<<endl;
+		cout<<"请你输入其他命令"<<endl;
+		return;
+	}
+
+	else
+	{
 	n=n+1;
+	cout<<"请你录入用户信息"<<endl;
 	cin>>p->id;
 	cin>>p->name;
 	cin>>p->gangwei;
 	cin>>p->xinji;
 	cin>>p->zhiwu;
 	cin>>p->jixiao;
-	
+	add_money(p);
+	grsds(p);
+	add_wages(p);
+	cout<<"职工信息为："<<endl;
+	cout<<"工号 "<<"姓名   "<<"岗位工资 "<<"薪级工资 "<<"职务津贴 "<<"绩效工资 "<<"应发工资 "<<"个人所得税 "<<"实发工资 "<<endl;
+	cout<<p->id<<" "<<p->name<<"      "<<p->gangwei<<"      "<<p->xinji<<"      "<<p->zhiwu<<"      ";
+	cout<<p->jixiao<<"      "<<p->yingfa<<"      "<<p->geren<<"      "<<p->shifa<<endl;
+	cout<<"请你进行下一步命令！"<<endl;
+	}
 }
-
-void grsds(zhigong *p1)//计算个人所得税函数
-{
-		if(p1->yingfa<500)                //应发工资不超过500
-			p1->geren=p1->yingfa*0.05;
-		else if(p1->yingfa<2000)                   //应发工资在500到2000
-			p1->geren=(p1->yingfa-2000)*0.1+500*0.05;
-		else if(p1->yingfa<5000)                                //应发工资在2000到5000
-			p1->geren=(p1->yingfa-5000)*0.15+1500*0.1+500*0.05;
-		else if(p1->yingfa<20000)                                          //应发工资在5000到20000
-			p1->geren=(p1->yingfa-20000)*0.2+15000*0.15+1500*0.1+500*0.05;
-		else if(p1->yingfa<40000)                                                     //应发工资在20000到40000
-			p1->geren=(p1->yingfa-40000)*0.25+20000*0.2+15000*0.15+1500*0.1+500*0.05;
-		else if(p1->yingfa<60000)                                                                 //应发工资在40000到60000
-			p1->geren=(p1->yingfa-60000)*0.3+20000*0.25+20000*0.2+15000*0.15+1500*0.1+500*0.05;
-		else if(p1->yingfa<80000)                                                                            //应发工资在60000到80000
-			p1->geren=(p1->yingfa-80000)*0.35+20000*0.3+20000*0.25+20000*0.2+15000*0.15+1500*0.1+500*0.05;
-		else if(p1->yingfa<100000)                                                                                   //应发工资在80000到100000
-			p1->geren=(p1->yingfa-80000)*0.4+20000*0.35+20000*0.3+20000*0.25+20000*0.2+15000*0.15+1500*0.1+500*0.05;
-		else                                                                                                                      //应发工资超过100000
-			p1->geren=(p1->yingfa-100000)*0.45+20000*0.4+20000*0.35+20000*0.3+20000*0.25+20000*0.2+15000*0.15+1500*0.1+500*0.05;
-}
-
 
 void add_money(zhigong *p1)									//计算应发工资       
 {
 		p1->yingfa=p1->gangwei+p1->xinji+p1->zhiwu+p1->jixiao;
 }
+
+void grsds(zhigong *p1)//计算个人所得税函数
+{
+		if(p1->yingfa<=500)                //应发工资不超过500
+			{
+				p1->geren=p1->yingfa*0.05;
+		    }
+
+		else if(p1->yingfa>500&&p1->yingfa<=2000)                   //应发工资在500到2000
+			{
+				p1->geren=(p1->yingfa-500)*0.1+500*0.05;
+            }
+
+		else if(p1->yingfa>2000&&p1->yingfa<=5000)                                //应发工资在2000到5000
+			{
+				p1->geren=(p1->yingfa-2000)*0.15+1500*0.1+500*0.05;
+		    }
+
+		else if(p1->yingfa>5000&&p1->yingfa<=20000)                                          //应发工资在5000到20000
+			{
+				p1->geren=(p1->yingfa-5000)*0.2+15000*0.15+1500*0.1+500*0.05;
+		    }
+
+		else if(p1->yingfa>20000&&p1->yingfa<=40000)                                                     //应发工资在20000到40000
+			{
+				p1->geren=(p1->yingfa-20000)*0.25+20000*0.2+15000*0.15+1500*0.1+500*0.05;
+		    }
+
+		else if(p1->yingfa>40000&&p1->yingfa<=60000)                                                                 //应发工资在40000到60000
+			{
+				p1->geren=(p1->yingfa-40000)*0.3+20000*0.25+20000*0.2+15000*0.15+1500*0.1+500*0.05;
+		    }
+
+		else if(p1->yingfa>60000&&p1->yingfa<=80000)                                                                            //应发工资在60000到80000
+			{
+				p1->geren=(p1->yingfa-60000)*0.35+20000*0.3+20000*0.25+20000*0.2+15000*0.15+1500*0.1+500*0.05;
+		    }
+
+		else if(p1->yingfa>80000&&p1->yingfa<=100000)                                                                                   //应发工资在80000到100000
+			{
+				p1->geren=(p1->yingfa-80000)*0.4+20000*0.35+20000*0.3+20000*0.25+20000*0.2+15000*0.15+1500*0.1+500*0.05;
+		    }
+
+		else                                                                                                                      //应发工资超过100000
+			{
+				p1->geren=(p1->yingfa-100000)*0.45+20000*0.4+20000*0.35+20000*0.3+20000*0.25+20000*0.2+15000*0.15+1500*0.1+500*0.05;
+		    }
+}
+
 
 void add_wages(zhigong *p1)									//计算实发工资	     
 {
