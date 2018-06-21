72ac2cdda313c307775238546d2d9387fb97c570
zf614032464 <40415550+zf614032464@users.noreply.github.com>
2018-06-21 21:44:10 +0800

    Add files via upload

--- zihanshu.cpp
+++ zihanshu.cpp
@@ -1,5 +1,5 @@
 #include"zhigong.h"
-void read(zhigong zggz[],int a)//文件读取
+void read(zhigong zggz[])//文件读取
 {
 	fstream f("gx.dat",ios::in|ios::binary);//以二进制方式打开一个输入文件
 	if(!f)
@@ -7,10 +7,18 @@ void read(zhigong zggz[],int a)//文件读取
 		cout<<"Cannot open file"<<endl;
 		abort();
 	}
-	for(int i=0;i<a;i++)
+	for(int i=0;i<100;i++)
 	{
+
+		if(!f.eof())//判断文件是否到达末尾 
+		{
+
 		n++;//职工人数加1
 		f>>zggz[i].id>>zggz[i].name>>zggz[i].gangwei>>zggz[i].xinji>>zggz[i].zhiwu>>zggz[i].jixiao>>zggz[i].yingfa>>zggz[i].geren>>zggz[i].shifa;
+		}//从文件读取数据到结构体数组
+
+		else
+			break;
 	}
 	f.close();
 }
@@ -32,33 +40,37 @@ void wirte(zhigong zggz[])
 
 	for(int i=0;i<n;i++)
 	{
-		f.write((char*)&zggz[i],sizeof(zggz[i]));
+		if(!f.eof())//判断文件是否到达末尾 
+		{
+		//f.write((char*)&zggz[i],sizeof(zggz[i]));
+		f<<zggz[i].id<<zggz[i].name<<zggz[i].gangwei<<zggz[i].xinji<<zggz[i].zhiwu<<zggz[i].jixiao<<zggz[i].yingfa<<zggz[i].geren<<zggz[i].shifa;
+		}
 	}
 	f.close();
     
 	/*zhigong zggz1[3];
 	fstream f1("gz.dat",ios::in|ios::binary);
-	if(!f)
+	if(!f1)
 	{
 		cout<<"Cannot open file"<<endl;
 		abort();
 	}
 	for(int i=0;i<n;i++)
 	{
-		//f.read((char*)&zggz1[i],sizeof(zggz1[i]));
-		f>>zggz1[i].id>>zggz1[i].name>>zggz1[i].gangwei>>zggz1[i].xinji>>zggz1[i].zhiwu>>zggz1[i].jixiao>>zggz1[i].yingfa>>zggz1[i].geren>>zggz1[i].shifa;
+		//f1.read((char*)&zggz[i],sizeof(zggz[i]));
+		f1>>zggz1[i].id>>zggz1[i].name>>zggz1[i].gangwei>>zggz1[i].xinji>>zggz1[i].zhiwu>>zggz1[i].jixiao>>zggz1[i].yingfa>>zggz1[i].geren>>zggz1[i].shifa;
 		cout<<zggz1[i].id<<" "<<zggz1[i].name<<"      "<<zggz1[i].gangwei<<"      "<<zggz1[i].xinji<<"      "<<zggz1[i].zhiwu<<"      ";
 			cout<<zggz1[i].jixiao<<"      "<<zggz1[i].yingfa<<"      "<<zggz1[i].geren<<"      "<<zggz1[i].shifa<<endl;
 	}
 	f1.close();*/
-	cout<<"请你进行下一步命令"<<endl;
+	cout<<"                      文件已保存，请你进行下一步命令"<<endl;
 }
 
 void find(zhigong zggz[])
 {//查询函数
 	string gohao;//定义字符串
 	int i;
-	cout<<"请输入你要查询的工号或姓名"<<endl;
+	cout<<"请输入你要查询的工号或姓名"<<endl;//通过工号或姓名进行查找
 	cin>>gohao;
 
 	for(i=0;i<n;i++)
@@ -67,19 +79,20 @@ void find(zhigong zggz[])
 		{
 			cout<<"此职工信息为："<<endl;
 			cout<<"工号 "<<"姓名   "<<"岗位工资 "<<"薪级工资 "<<"职务津贴 "<<"绩效工资 "<<"应发工资 "<<"个人所得税 "<<"实发工资 "<<endl;
-			add_money(&zggz[i]);
-			grsds(&zggz[i]);
-			add_wages(&zggz[i]);
+			add_money(&zggz[i]);//更正职工应发工资
+			grsds(&zggz[i]);//更正职工个人所得税
+			add_wages(&zggz[i]);//更正职工实发工资
 			cout<<zggz[i].id<<" "<<zggz[i].name<<"      "<<zggz[i].gangwei<<"      "<<zggz[i].xinji<<"      "<<zggz[i].zhiwu<<"      ";
 			cout<<zggz[i].jixiao<<"      "<<zggz[i].yingfa<<"      "<<zggz[i].geren<<"      "<<zggz[i].shifa<<endl;
-			cout<<"请你进行下一步命令！"<<endl;
+			cout<<"                              请你进行下一步命令！"<<endl;
 			break;
 		}
 	}
 
 	if(n==i)
 	{
-		cout<<"系统中找不到此职工信息，请你核实你输入的工号是否有错，请重新输入查询命令再进行查询"<<endl;
+		cout<<"系统中找不到此职工信息，请你核实你输入的工号或姓名是否有错，请重新输入查询命令再进行查询"<<endl;
+		return;
 	}
 }
 
@@ -92,41 +105,93 @@ void list(zhigong zggz[])
 		cout<<zggz[i].id<<" "<<zggz[i].name<<"      "<<zggz[i].gangwei<<"      "<<zggz[i].xinji<<"      "<<zggz[i].zhiwu<<"      ";
 		cout<<zggz[i].jixiao<<"      "<<zggz[i].yingfa<<"      "<<zggz[i].geren<<"      "<<zggz[i].shifa<<endl;
 	}
-	cout<<"请你进行下一步命令！"<<endl;
+	cout<<"                    请你进行下一步命令！"<<endl;
 }
 
 void modify(zhigong zggz[])
 {//修改函数
 	string gohao;//定义字符串
-	int i;
-	cout<<"请输入你要查询的工号或姓名"<<endl;
+	int i,k;
+	zhigong a;
+	cout<<"请输入你要修改的工号或姓名"<<endl;//通过工号或姓名进行查找
     cin>>gohao;
 
 	for(i=0;i<n;i++)
 	{
 		if(zggz[i].id==gohao||zggz[i].name==gohao)
 		{
+			cout<<"职工信息为："<<endl;//输出职工修改信息前的信息
+	        cout<<"工号 "<<"姓名   "<<"岗位工资 "<<"薪级工资 "<<"职务津贴 "<<"绩效工资 "<<"应发工资 "<<"个人所得税 "<<"实发工资 "<<endl;
+			cout<<zggz[i].id<<" "<<zggz[i].name<<"      "<<zggz[i].gangwei<<"      "<<zggz[i].xinji<<"      "<<zggz[i].zhiwu<<"      ";
+		    cout<<zggz[i].jixiao<<"      "<<zggz[i].yingfa<<"      "<<zggz[i].geren<<"      "<<zggz[i].shifa<<endl;
 
+			cout<<"输入1表示确认修改，输入0表示放弃修改"<<endl;
+			cin>>k;
+			if(k==1)
+			{
+				cout<<"请重新录入职工工号、姓名、岗位工资、薪级工资、职务津贴、绩效工资"<<endl;
+				cout<<"工号：";
+				cin>>a.id;//工号
+				cout<<"姓名：";
+				cin>>a.name;//姓名
+				cout<<"岗位工资：";
+				cin>>a.gangwei;//岗位工资
+				cout<<"薪级工资：";
+				cin>>a.xinji;//薪级工资
+				cout<<"职务津贴：";
+				cin>>a.zhiwu;//职务津贴
+				cout<<"绩效工资：";
+				cin>>a.jixiao;//绩效工资
+				zggz[i].id=a.id;
+				zggz[i].name=a.name;
+				zggz[i].gangwei=a.gangwei;
+				zggz[i].xinji=a.xinji;
+				zggz[i].zhiwu=a.zhiwu;
+				zggz[i].jixiao=a.jixiao;
+				add_money(&zggz[i]);//职工应发工资
+			    grsds(&zggz[i]);//职工个人所得税
+			    add_wages(&zggz[i]);//职工实发工资
+
+				cout<<"职工信息修改后为："<<endl;//输出职工修改信息后的信息
+	            cout<<"工号 "<<"姓名   "<<"岗位工资 "<<"薪级工资 "<<"职务津贴 "<<"绩效工资 "<<"应发工资 "<<"个人所得税 "<<"实发工资 "<<endl;
+			    cout<<zggz[i].id<<" "<<zggz[i].name<<"      "<<zggz[i].gangwei<<"      "<<zggz[i].xinji<<"      "<<zggz[i].zhiwu<<"      ";
+		        cout<<zggz[i].jixiao<<"      "<<zggz[i].yingfa<<"      "<<zggz[i].geren<<"      "<<zggz[i].shifa<<endl;
+				cout<<"                     职工信息已修改，请进行下一步命令。"<<endl;
+			    return;
+			}
+
+			else
+			{
+				cout<<"                    请你进行下一步命令！"<<endl;//放弃修改后的操作
+				return;
+			}
 		}
 	}
 
 	if(n==i)
 	{
-		cout<<"系统中找不到此职工信息，请你核实你输入的工号是否有错，请重新输入查询命令再进行查询"<<endl;
+		cout<<"系统中找不到此职工信息，请你核实你输入的工号或姓名是否有错，请重新输入查询命令再进行查询"<<endl;
 	}
+
+	cout<<"                    请你进行下一步命令！"<<endl;
 }
 
 void del(zhigong zggz[])//删除函数
 {
 	string gohao;//定义字符串
 	int i,j,k;
-	cout<<"请输入你要修改的工号或姓名"<<endl;
+	cout<<"请输入你要删除的工号或姓名"<<endl;//通过工号或姓名进行查找
     cin>>gohao;
 
 	for(i=0;i<n;i++)
 	{
 		if(zggz[i].id==gohao||zggz[i].name==gohao)
 		{
+			cout<<"删除的职工信息为："<<endl;//输出职工修改信息前的信息
+	        cout<<"工号 "<<"姓名   "<<"岗位工资 "<<"薪级工资 "<<"职务津贴 "<<"绩效工资 "<<"应发工资 "<<"个人所得税 "<<"实发工资 "<<endl;
+			cout<<zggz[i].id<<" "<<zggz[i].name<<"      "<<zggz[i].gangwei<<"      "<<zggz[i].xinji<<"      "<<zggz[i].zhiwu<<"      ";
+		    cout<<zggz[i].jixiao<<"      "<<zggz[i].yingfa<<"      "<<zggz[i].geren<<"      "<<zggz[i].shifa<<endl;
+
 			cout<<"输入1表示删除，输入0表示放弃删除"<<endl;
 			cin>>k;
 			if(k==1)
@@ -136,11 +201,22 @@ void del(zhigong zggz[])//删除函数
 					zggz[j]=zggz[j+1];
 				}
 			n=n-1;
-			break;
+
+			cout<<"删除后剩余职工的信息为："<<endl;
+			cout<<"工号 "<<"姓名   "<<"岗位工资 "<<"薪级工资 "<<"职务津贴 "<<"绩效工资 "<<"应发工资 "<<"个人所得税 "<<"实发工资 "<<endl;
+			for(i=0;i<n;i++)
+			{
+				cout<<zggz[i].id<<" "<<zggz[i].name<<"      "<<zggz[i].gangwei<<"      "<<zggz[i].xinji<<"      "<<zggz[i].zhiwu<<"      ";
+		        cout<<zggz[i].jixiao<<"      "<<zggz[i].yingfa<<"      "<<zggz[i].geren<<"      "<<zggz[i].shifa<<endl;
 			}
+
+			cout<<"                      请你进行下一步命令！"<<endl;
+			return;
+			}
+
 			else
 			{
-				cout<<"请你进行下一步命令！"<<endl;
+				cout<<"                    请你进行下一步命令！"<<endl;//放弃删除后的操作
 				return;
 			}
 		}
@@ -148,38 +224,45 @@ void del(zhigong zggz[])//删除函数
 
 	if(n==i)
 	{
-		cout<<"系统中找不到此职工信息，请你核实你输入的工号是否有错，请重新输入查询命令再进行查询"<<endl;
+		cout<<"系统中找不到此职工信息，请你核实你输入的工号或姓名是否有错，请重新输入查询命令再进行查询"<<endl;
 	}
-	cout<<"请你进行下一步命令！"<<endl;
+	cout<<"                      请你进行下一步命令！"<<endl;
 }
 
 void add(zhigong *p)//增加函数
 {
 	if(n==100)
 	{
-		cout<<"职工人数已满，不能添加职工了"<<endl;
-		cout<<"请你输入其他命令"<<endl;
+		cout<<"          职工人数已满，不能添加职工了"<<endl;
+		cout<<"                    请你输入其他命令"<<endl;
 		return;
 	}
 
 	else
 	{
+	p=p+n;//把指针移到数组末尾
 	n=n+1;
 	cout<<"请你录入用户信息"<<endl;
-	cin>>p->id;
-	cin>>p->name;
-	cin>>p->gangwei;
-	cin>>p->xinji;
-	cin>>p->zhiwu;
-	cin>>p->jixiao;
-	add_money(p);
-	grsds(p);
-	add_wages(p);
+	cout<<"工号：";
+	cin>>p->id;//工号
+	cout<<"姓名：";
+	cin>>p->name;//姓名
+	cout<<"岗位工资：";
+	cin>>p->gangwei;//岗位工资
+	cout<<"薪级工资：";
+	cin>>p->xinji;//薪级工资
+	cout<<"职务津贴：";
+	cin>>p->zhiwu;//职务津贴
+	cout<<"绩效工资：";
+	cin>>p->jixiao;//绩效工资
+	add_money(p);//应发工资
+	grsds(p);//个人所得税
+	add_wages(p);//实发工资
 	cout<<"职工信息为："<<endl;
 	cout<<"工号 "<<"姓名   "<<"岗位工资 "<<"薪级工资 "<<"职务津贴 "<<"绩效工资 "<<"应发工资 "<<"个人所得税 "<<"实发工资 "<<endl;
 	cout<<p->id<<" "<<p->name<<"      "<<p->gangwei<<"      "<<p->xinji<<"      "<<p->zhiwu<<"      ";
 	cout<<p->jixiao<<"      "<<p->yingfa<<"      "<<p->geren<<"      "<<p->shifa<<endl;
-	cout<<"请你进行下一步命令！"<<endl;
+	cout<<"                        请你进行下一步命令！"<<endl;
 	}
 }
 
